185f31c522d22f4338780668d075a7647b18244e
Gustafa Bayu <faafa13@gmail.com>
2023-07-05 15:20:15 +0700

    forth commit - admin panel page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
 
 class AdminController extends Controller
 {
-    public function AdminDashboard(){
-        return view('admin.admin-dashboard');
+    public function AdminDashboard()
+    {
+        return view('admin.index');
+    }
+
+    public function AdminLogout(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
     }
 }
